d3f082efec765cf13a89c3818669352a3322929d
Tara Kadir Schön <115081104+TaraKadir@users.noreply.github.com>
2025-09-23 21:27:12 +0200

    chore: bytte till HashRouter för GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { CountryProvider } from "./context/CountryContext.jsx";
 import App from "./App.jsx";
 import "./styles/styles.css";
@@ -8,9 +8,9 @@ import "./styles/styles.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CountryProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </CountryProvider>
   </React.StrictMode>
 );
